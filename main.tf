0884d8a4998716ddf2150c8c3591affbf1503be6
Benoit Blais <benoit.blais@hashicorp.com>
2025-11-13 08:38:01 -0500

    add add-on

--- main.tf
+++ main.tf
@@ -10,15 +10,31 @@ data "tfe_project" "this" {
 }
 
 resource "hcp_waypoint_template" "this" {
-  name                            = var.template_name
-  summary                         = var.template_summary
-  terraform_project_id            = data.tfe_project.this.id
-  terraform_no_code_module_source = data.tfe_registry_module.this.no_code_module_source
+  name                            = var.name
   terraform_no_code_module_id     = data.tfe_registry_module.this.no_code_module_id
+  terraform_no_code_module_source = data.tfe_registry_module.this.no_code_module_source
+  terraform_project_id            = data.tfe_project.this.id
+  summary                         = var.template_summary
   description                     = var.template_description
-  labels                          = var.template_labels
-  # readme_markdown_template        = fileexists(var.template_readme_path) ? file(var.template_readme_path) : null
-  variable_options = var.template_variables
+  labels                          = var.labels
+  # readme_markdown_template        = fileexists(var.readme_path) ? file(var.readme_path) : null
+  variable_options = var.variables
+
+  lifecycle {
+    ignore_changes = [readme_markdown_template]
+  }
+}
+
+resource "hcp_waypoint_add_on_definition" "this" {
+  name                            = var.name
+  description                     = var.add_on_description
+  summary                         = var.add_on_summary
+  terraform_no_code_module_id     = data.tfe_registry_module.this.no_code_module_id
+  terraform_no_code_module_source = data.tfe_registry_module.this.no_code_module_source
+  terraform_project_id            = data.tfe_project.this.id
+  labels                          = var.labels
+  # readme_markdown_template        = fileexists(var.readme_path) ? file(var.readme_path) : null
+  variable_options = var.variables
 
   lifecycle {
     ignore_changes = [readme_markdown_template]
